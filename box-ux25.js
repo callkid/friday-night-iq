e4e9def1eb78011d333c7b9c0526ed08142122f0
callkid <callemmett@gmail.com>
2026-08-25 10:59:17 -0600

    Add Quality25 box ergonomics layer

---
+++ box-ux25.js
@@ -0,0 +1,7 @@
+(function(root){
+'use strict';
+var d=root.document,A=root.FNIQ;if(!d||!A||A.__boxUx25)return;A.__boxUx25=true;
+function inject(){if(d.getElementById('boxUx25Css'))return;var l=d.createElement('link');l.id='boxUx25Css';l.rel='stylesheet';l.href='box-ux25.css?v=boxux25';d.head.appendChild(l);}
+d.documentElement.classList.add('fniqBoxUx25');
+inject();
+})(window);
